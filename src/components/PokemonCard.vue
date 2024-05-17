c6362026cd9c4f7c32b00a8d744986be67413147
Roger Acosta <roger@mailvibes.io>
2024-05-17 12:47:10 -0600

    Added styles to cards and responsive pokemon list

--- src/components/PokemonCard.vue
+++ src/components/PokemonCard.vue
@@ -6,16 +6,16 @@ const handleClick = () => {
 </script>
 
 <template>
-  <section class='pokemonCard cursor-pointer' style={styleBg} @click="handleClick">
+  <section class='relative h-full w-full max-h-36 max-w-56 pt-3.5 pr-0 pb-2 pl-3 overflow-hidden border border-gray-200 rounded-lg shadow cursor-pointer' @click="handleClick">
     <h2 class='capitalize font-bold tracking-tighter md:text-lg text-sm text-gray-900'>
       {{name}}
     </h2>
-    <h3>#{{String(`${id}`).padStart(3, "0")}}</h3>
+    <h3 class="opacity-55 absolute right-2 top-1 text-xs font-bold md:right-3 md:top-2 md:text-base">#{{String(`${id}`).padStart(3, "0")}}</h3>
     <figure class='flex items-center justify-center'>
       <div class='w-1/2 relative'>
-        <img class='pokemonImg'
+        <img class='relative h-auto right-1 w-full'
           :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`"
-          alt="name" />
+          :alt="name" />
       </div>
     </figure>
   </section>
--- src/components/PokemonList.vue
+++ src/components/PokemonList.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 import { usePokemonStore } from '@/stores/pokemonStore';
 import PokemonCard from './PokemonCard.vue';
+import { storeToRefs } from 'pinia';
 
-const { pokemons } = usePokemonStore()
+const pokemonStore = usePokemonStore()
+const { pokemons } = storeToRefs(pokemonStore)
 </script>
 
 <template>
-  <section class='grid grid-cols-2 justify-items-center gap-x-2 md:gap-x-4 md:gap-y-5 gap-y-2 md:w-11/12 w-full mx-auto pb-10 z-10'>
+  <section class='grid grid-cols-2 xl:grid-cols-5 md:grid-cols-4 justify-items-center gap-x-2 md:gap-x-4 md:gap-y-5 gap-y-2 md:w-11/12 w-full mx-auto pb-10 z-10'>
     <PokemonCard
       v-for="(pokemon, idx) in pokemons"
       :key="idx"
